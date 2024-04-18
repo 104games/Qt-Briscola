9be8d2465149bf68d2402bcdd757b5f21bdfd749
104 games <104games.studio@gmail.com>
2024-04-18 22:28:41 +0200

    cooking

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<vector>
+#include<iostream>
 using namespace std;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,17 +15,46 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+/*
+ * semi:
+ *      1 = denari
+ *      2 = bastoni
+ *      3 = spade
+ *      4 = coppe
+ */
+
+class Carta{
+    public:
+    int num;
+    int seme;
+    int punti;
+    string texture;
+    Carta(int n, int s, int p, string t){
+        num = n;
+        seme = s;
+        punti = p;
+        texture = t;
+    }
+};
 
 
 
-void MainWindow::on_carta_clicked()
+vector<Carta> mazzo;
+
+void riempi_mazzo(){
+    Carta ReDenari =  Carta(10,1,4,":/carte/carte/10d.jpg");
+    Carta CinqueSpade = Carta(5,3,0,":/carte/carte/5s.jpg");
+    mazzo.push_back(ReDenari);
+    mazzo.push_back(CinqueSpade);
+    cout << mazzo[0].texture << endl << mazzo[1].texture << endl;
+}
+
+
+
+
+
+void MainWindow::on_mod1_clicked()
 {
-    if(ui->carta->styleSheet()=="image: url(:/carte/carte/5s.jpg);"){
-        ui->carta->setStyleSheet("image: url(:/carte/carte/10d.jpg);");
-    }
-    else{
-        ui->carta->setStyleSheet("image: url(:/carte/carte/5s.jpg);");
-    }
-
+    riempi_mazzo();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -24,6 +24,8 @@ private slots:
 
     void on_carta_clicked();
 
+    void on_mod1_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
